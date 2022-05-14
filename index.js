6ab3fb37ed22a57c3c2f05ad7f6b0bb83efb8b74
Warze <40837031+WarzeEgg@users.noreply.github.com>
2022-05-14 22:41:59 +0200

    Adjusted dialogue functions

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const dialogue = document.getElementById('textDialogue')
 
-function writeDialogueText(string) {
-	let writtenLetters = 0;
-	dialogue.innerHTML = ""
-	const interval = setInterval(function() {
-		if (string[writtenLetters] == " ") {
-			writtenLetters ++;
-			dialogue.innerHTML += " "
-		}
-		dialogue.innerHTML += `<span>${string[writtenLetters]}</span`
-		writtenLetters ++;
-		if (writtenLetters == string.length) {
-			clearInterval(interval);
-		}
-	}, 100);
+const rainbow_colors = ["#F00","#F20","#F40","#F60","#F80","#FA0","#FC0","#FE0","#EF0","#CF0","#AF0","#8F0","#6F0","#4F0","#2F0"]
+
+function calcRainbow(input) {
+	const r = "D"
+	const g = (input % 16).toString(16)
+	const b = "0"
+	return "#" + r + g + b
 }
 
-writeDialogueText("Hello there")
+function writeDialogueText(string) {
+	return new Promise(resolve => {
+		let writtenLetters = 0;
+		dialogue.innerHTML = ""
+		const interval = setInterval(function() {
+			if (string[writtenLetters] == " ") {
+				writtenLetters ++;
+				dialogue.innerHTML += " "
+			}
+			dialogue.innerHTML += `<span style="text-shadow:4px 0px ${calcRainbow(writtenLetters)}">${string[writtenLetters]}</span`
+			writtenLetters ++;
+			if (writtenLetters == string.length) {
+				clearInterval(interval);
+				setTimeout(resolve, 1000)
+			}
+		}, 100);
+	});
+}
+
+async function startDialogueProcess() {
+	await writeDialogueText("Hello there")
+	await writeDialogueText("Cool animation")
+	await writeDialogueText("Look at him go")
+	await writeDialogueText("Aight thanks")
+}
+
+startDialogueProcess();
